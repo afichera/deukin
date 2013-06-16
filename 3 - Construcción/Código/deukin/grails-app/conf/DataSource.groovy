2c25ebbcc43c7cd7b1d0861edd0c76267e6a260a
Alejandro Fichera <alejandrofichera@gmail.com>
2013-06-16 16:34:14 -0300

    Cambios en el DataSource para que apunte a PostgresSQL

--- 3 - Construcción/Código/deukin/grails-app/conf/DataSource.groovy
+++ 3 - Construcción/Código/deukin/grails-app/conf/DataSource.groovy
@@ -1,43 +1,51 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	pooled = true
+	//    driverClassName = "org.h2.Driver"
+	driverClassName = "org.postgresql.Driver"
+	//	url = "jdbc:postgresql://localhost:5432/grails"
+	//    username = "sa"
+	//    password = ""
+	//	dialect = "net.sf.hibernate.dialect.PostgreSQLDialect"
+	username = "postgres"
+	password = "postgres"
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
+	development {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = 'jdbc:postgresql://localhost:5432/deukin_dev_db'
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "create"
+			//            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = 'jdbc:postgresql://localhost:5432/deukin_test_db'
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "create"
+			//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = 'jdbc:postgresql://localhost:5432/deukin_prod_db'
+			pooled = true
+			properties {
+				maxActive = -1
+				minEvictableIdleTimeMillis=1800000
+				timeBetweenEvictionRunsMillis=1800000
+				numTestsPerEvictionRun=3
+				testOnBorrow=true
+				testWhileIdle=true
+				testOnReturn=true
+				validationQuery="SELECT 1"
+			}
+		}
+	}
 }

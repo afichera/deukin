7ee0f5ea6f0344785ea500f813d59f2284c4156c
Alejandro Fichera <alejandrofichera@gmail.com>
2013-06-16 16:30:27 -0300

    Cambios de Configuración en Plugins, etc

--- 3 - Construcción/Código/deukin/grails-app/conf/BuildConfig.groovy
+++ 3 - Construcción/Código/deukin/grails-app/conf/BuildConfig.groovy
@@ -2,8 +2,8 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
@@ -26,15 +26,21 @@ grails.project.dependency.resolution = {
         mavenCentral()
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
+		
+		mavenRepo "http://repository.springsource.com/maven/bundles/release"
+		mavenRepo "http://repository.springsource.com/maven/bundles/external"
+		mavenRepo "http://repository.springsource.com/maven/libraries/release"
+		mavenRepo "http://repository.springsource.com/maven/libraries/external"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.20'
+		runtime 'postgresql:postgresql:9.1-901.jdbc4'
     }
 
     plugins {
@@ -43,14 +49,18 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.1.6"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
+        runtime ":zipped-resources:1.0"
+        runtime ":cached-resources:1.0"
+        runtime ":yui-minify-resources:0.1.4"
+		
 
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.1"
-
+		
+		compile ":cache-headers:1.1.5"
         compile ':cache:1.0.0'
+		compile ":mail:1.0.1"
+		compile ":spring-security-core:1.2.7.1"
     }
 }

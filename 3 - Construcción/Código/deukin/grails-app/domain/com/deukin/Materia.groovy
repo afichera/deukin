e9a4d3d0ea128c3dfac02d702b3202be51f0836a
Alejandro Fichera <alejandrofichera@gmail.com>
2013-06-26 20:37:22 -0300

    Modificaciones en Modelo

--- 3 - Construcción/Código/deukin/grails-app/domain/com/deukin/Materia.groovy
+++ 3 - Construcción/Código/deukin/grails-app/domain/com/deukin/Materia.groovy
@@ -1,6 +1,23 @@
 package com.deukin
 
+import gorm.AuditStamp;
+
+/**
+ * Representa una Materia de la {@link Institucion}
+ * @author Ale Mobile
+ *
+ */
+@AuditStamp
 class Materia {
+	
+	Integer codigo
+	String nombre
+	Integer cargaHoraria
+	//Integer cupoMaximoAlumnos No estoy seguro que esta condición la de una Materia, creo que es de Curso pero en la propuesta aparece así.
+	TextoLibre contenidoMinimo
+	Integer cantidadDocentesRequeridos
+	static hasMany = [objetivosGenerales:TextoLibre, objetivosEspecificos:TextoLibre, recursos:Recurso]
+	TextoLibre bibliografia
 
     static constraints = {
     }

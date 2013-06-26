a3926176efb1e50423305f18c11f520540ecb761
Alejandro Fichera <alejandrofichera@gmail.com>
2013-06-26 20:49:30 -0300

    Agrego comentario

--- 3 - Construcción/Código/deukin/grails-app/domain/com/deukin/Materia.groovy
+++ 3 - Construcción/Código/deukin/grails-app/domain/com/deukin/Materia.groovy
@@ -9,16 +9,17 @@ import gorm.AuditStamp;
  */
 @AuditStamp
 class Materia {
-	
+
 	Integer codigo
 	String nombre
 	Integer cargaHoraria
 	//Integer cupoMaximoAlumnos No estoy seguro que esta condición la de una Materia, creo que es de Curso pero en la propuesta aparece así.
 	TextoLibre contenidoMinimo
 	Integer cantidadDocentesRequeridos
+	//Tenemos que definir si las coorrelatividades y equivalencias son una coleccion de Materia o ibamos a crear otra cosa que las agrupe.
 	static hasMany = [objetivosGenerales:TextoLibre, objetivosEspecificos:TextoLibre, recursos:Recurso]
 	TextoLibre bibliografia
 
-    static constraints = {
-    }
+	static constraints = {
+	}
 }

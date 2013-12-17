eaa6bef03e44519658bbcd19b6a36670e900cbe4
Alejandro Fichera <alejandrofichera@gmail.com>
2013-12-16 22:30:57 -0300

    Valido que la equivalencia no venga nula.
    Si es nula el controller se encargará de mostrar el error.

--- 3 - Construcción/Código/deukin/grails-app/services/com/deukin/EquivalenciaService.groovy
+++ 3 - Construcción/Código/deukin/grails-app/services/com/deukin/EquivalenciaService.groovy
@@ -5,16 +5,15 @@ import com.deukin.exceptions.BusinessException
 class EquivalenciaService {
 	
 	def validar(Equivalencia futuraEquivalencia){
-		
-		if(futuraEquivalencia?.materiaPrincipal?.planEstudio.equals(futuraEquivalencia?.materiaEquivalente?.planEstudio)){
-			throw new BusinessException('No se puede crear equivalencia entre materias del mismo Plan de estudio!')
-		}
-		
-		def equivalenciaExistente = Equivalencia.findByMateriaPrincipalAndMateriaEquivalente(futuraEquivalencia.materiaPrincipal, futuraEquivalencia.materiaEquivalente)
-		if(equivalenciaExistente){
-			throw new BusinessException("La equivalencia que intenta crear ya existe!")
+		if(futuraEquivalencia?.materiaPrincipal && futuraEquivalencia?.materiaEquivalente){
+			if(futuraEquivalencia?.materiaPrincipal?.planEstudio.equals(futuraEquivalencia?.materiaEquivalente?.planEstudio)){
+				throw new BusinessException('No se puede crear equivalencia entre materias del mismo Plan de estudio!')
+			}
+			
+			def equivalenciaExistente = Equivalencia.findByMateriaPrincipalAndMateriaEquivalente(futuraEquivalencia.materiaPrincipal, futuraEquivalencia.materiaEquivalente)
+			if(equivalenciaExistente){
+				throw new BusinessException("La equivalencia que intenta crear ya existe!")
+			}
 		}		
-
-		
 	}
 }

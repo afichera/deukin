cd0d42209f48e2c8f28c28e70cdfea4d7088f83e
Alejandro Fichera <alejandrofichera@gmail.com>
2013-12-20 12:16:58 -0300

    Me habia chispoteado puntos y comas..

--- 3 - Construcción/Código/deukin/grails-app/services/com/deukin/UploadService.groovy
+++ 3 - Construcción/Código/deukin/grails-app/services/com/deukin/UploadService.groovy
@@ -18,15 +18,15 @@ class UploadService {
 	 * @return 
 	 */
 	def validateTextFileFormat(String fileName){
-		int a = fileName.lastIndexOf(".");
-		String extName = fileName.substring(a).toUpperCase();
-		ArrayList<String> extList = new ArrayList<String>();
-		extList.add(".TXT");
-		extList.add(".ODT");
-		extList.add(".RTF");
-		extList.add(".DOC");
-		extList.add(".DOCX");
-		extList.add(".PDF");
+		int a = fileName.lastIndexOf(".")
+		String extName = fileName.substring(a).toUpperCase()
+		ArrayList<String> extList = new ArrayList<String>()
+		extList.add(".TXT")
+		extList.add(".ODT")
+		extList.add(".RTF")
+		extList.add(".DOC")
+		extList.add(".DOCX")
+		extList.add(".PDF")
 		if(!extList.contains(extName)) {
 			throw new BusinessException('El formato de Archivo es inválido. Debe ser un documento de texto válido. Ej: doc, docx, txt, rtf, odt, pdf.')
 		}
@@ -41,12 +41,12 @@ class UploadService {
 	 * @return
 	 */
 	def validateImageFileFormat(String fileName){
-		int a = fileName.lastIndexOf(".");
-		String extName = fileName.substring(a).toUpperCase();
-		ArrayList<String> extList = new ArrayList<String>();
-		extList.add(".JPG");
-		extList.add(".JPEG");
-		extList.add(".PNG");
+		int a = fileName.lastIndexOf(".")
+		String extName = fileName.substring(a).toUpperCase()
+		ArrayList<String> extList = new ArrayList<String>()
+		extList.add(".JPG")
+		extList.add(".JPEG")
+		extList.add(".PNG")
 		if(!extList.contains(extName)) {
 			throw new BusinessException('El formato de Archivo es inválido. Debe ser una imágen válida Ej: jpg, jpeg, png.')
 		}
